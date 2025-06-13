9795712a019a026adc3479b1640f4ee0fddb0af6
Ladislau Lopes <ladislau.contato.01@gmail.com>
2025-06-13 10:41:28 -0500

    autenticação feita

--- auth/user_auth.py
+++ auth/user_auth.py
@@ -6,6 +6,10 @@ from datetime import datetime, timedelta
 from pytz import timezone
 from dotenv import load_dotenv
 import os
+from fastapi.responses import JSONResponse
+from jose.exceptions import ExpiredSignatureError, JWTError
+
+
 load_dotenv()
 
 def _criar_token(tipo_token:str,tempo_vida:timedelta,sub:str):
@@ -31,3 +35,43 @@ def criar_token_acesso(sub:str):
         tempo_vida=timedelta(minutes=30),
         sub=sub
     )
+
+def criar_token_refresh(sub: str):
+    return _criar_token(
+        tipo_token="refresh_token",
+        tempo_vida=timedelta(days=7),  # ou outro tempo desejado
+        sub=sub
+    )
+
+
+def gerar_resposta_autenticao(user_id:str):
+    """Função que gera a resposta de autenticação com o token"""
+    access_token  = criar_token_acesso(sub=user_id)
+    refresh_token = criar_token_refresh(sub=user_id)
+    response = JSONResponse(content={
+        "message": "Login realizado com sucesso!",
+        "access_token": access_token ,
+        "refresh_token": refresh_token
+    })
+    response.set_cookie(key='access_token',
+                        value=access_token ,
+                        httponly=True,
+                        max_age=1800,  # 30 minutos
+                        samesite='lax',
+                        secure=True)  # apenas em HTTPS
+    response.set_cookie(key='refresh_token',
+                        value=refresh_token,
+                        httponly=True,
+                        max_age=604800,  # 7 dias
+                        samesite='lax',
+                        secure=True)  # apenas em HTTPS
+    return response
+
+def verificar_token(token: str):
+    try:
+        payload = jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=[os.getenv("ALGORITHM")])
+        return payload.get("sub")
+    except ExpiredSignatureError:
+        raise ExpiredSignatureError("Token expirado")
+    except JWTError:
+        return None
--- router/user_route.py
+++ router/user_route.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException , Response
+from fastapi import APIRouter, Depends, HTTPException ,Request
 from sqlalchemy.orm import Session
 from controller.user_controler import user_creator_service , get_user_by_email_controler
 from database.conexao_banco import get_db
 from schemas.user_schemas import UsuarioSchemas
 from models.user_model import Usuario
-from auth.user_auth import criar_token_acesso
-
+from auth.user_auth import gerar_resposta_autenticao , verificar_token
+from jose.exceptions import ExpiredSignatureError, JWTError
 route = APIRouter(prefix='/user',tags=['Usuario'])
 
 @route.post('/create_user')
@@ -15,26 +15,61 @@ def user_creator(user:UsuarioSchemas,db:Session = Depends(get_db)):
     except Exception as e:
         raise HTTPException(status_code=422, detail=str(e))
     
+
 @route.get('/auth')
-def login(email: str, password: str, db: Session = Depends(get_db), response: Response = Response()):
+def login(email: str, password: str, db: Session = Depends(get_db)):
     try:
         user = get_user_by_email_controler(email, db)
+        if not user:
+            raise HTTPException(status_code=404, detail="Usuário não encontrado")
         auth = Usuario.verificar_senha_bcrypt(senha_fornecida=password, hash_armazenado=user.senha)
-        
+
         if not auth:
             raise HTTPException(status_code=401, detail="Senha incorreta")
-        
-        token = criar_token_acesso(sub=user.id)
 
-        response.set_cookie(
-            key='access_token',
-            value=token,
-            httponly=True,
-            max_age=1800,  # 30 minutos
-            samesite='lax',
-            secure=True  # apenas em HTTPS
-        )
+        return gerar_resposta_autenticao(user_id=user.id)
 
-        return {"mensagem": "Login realizado com sucesso!", "token": token}
     except Exception as e:
         raise HTTPException(status_code=401, detail=f"Erro na autenticação: {str(e)}")
+
+
+
+
+@route.get('/verify_login')
+def verify_login(request: Request):
+    access_token = request.cookies.get('access_token')
+    refresh_token = request.cookies.get('refresh_token')
+
+    if not access_token or not refresh_token:
+        raise HTTPException(status_code=401, detail="Usuário não autenticado")
+
+    try:
+        # Primeiro tenta validar o access token
+        user_id = verificar_token(access_token)
+        if user_id:
+            return {"mensagem": "Usuário autenticado", "user_id": user_id}
+
+    except ExpiredSignatureError:
+        # Access token expirou — tenta validar o refresh
+        try:
+            user_id = verificar_token(refresh_token)
+            if not user_id:
+                raise HTTPException(status_code=401, detail="Refresh token inválido")
+
+            # Gera novo access token e atualiza o cookie
+
+            return gerar_resposta_autenticao(user_id=user_id)
+
+        except JWTError:
+            raise HTTPException(status_code=401, detail="Refresh token inválido")
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Access token inválido")
+
+    # Fallback
+    raise HTTPException(status_code=401, detail="Erro ao validar tokens")
+
+
+@route.get('/teste')
+def rota_protegida(user_id: str = Depends(verify_login)):
+    return {"message": "Alo mundo", "user_id": user_id}
